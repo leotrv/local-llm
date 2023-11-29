12039d5fa8aff91c7412ea61186f9ca91fc0fb6c
leotrv <leo@traven.de>
2023-11-29 19:54:02 +0100

    add frontend

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
 from llama_cpp import Llama
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+import streamlit as st
+import box
+import yaml
 
+
+# Import config vars
+with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
+    cfg = box.Box(yaml.safe_load(ymlfile))
 
 llm = Llama(model_path="/home/leotraven/Development/llms/TheBloke/Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q2_K.gguf", chat_format="llama-2")
-result = llm.create_chat_completion(
-      messages = [
-          {"role": "system", "content": "You are an assistant who perfectly describes images."},
-          {
-              "role": "user",
-              "content": "Describe this image in detail please."
-          }
-      ],
-      stream=True
-)
-print(result["choices"][0]["message"]["content"])
+
+st.title("Document chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
+    db3 = Chroma(persist_directory=cfg.CHROMA_PATH, embedding_function=embeddings)
+    docs = db3.similarity_search(prompt)
+    context = "\n\n".join(doc.page_content for doc in docs)
+
+    for response in  llm.create_chat_completion(
+        messages = [
+            {"role": "system", "content": "You are an assistant who answers questions about documents. Be concise and do not use more than 3 sentences. You are given a context: {context}"},
+            {"role": "user", "content": prompt}
+        ],
+        stream=True
+        ):
+            print(response)
+            full_response += (response.get("choices")[0].get("delta").get("content") or "")
+            message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
