c45948af97d082a7eef5177ca21ea8d92be9a15d
leotrv <leo@traven.de>
2023-11-29 20:14:25 +0100

    create vectordb_build.py

---
+++ vectordb_build.py
@@ -0,0 +1,29 @@
+import box
+import yaml
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+
+# Import config vars
+with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
+    cfg = box.Box(yaml.safe_load(ymlfile))
+
+
+# Build vector database
+def run_db_build():
+    loader = DirectoryLoader(cfg.DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=cfg.CHUNK_SIZE,
+                                                   chunk_overlap=cfg.CHUNK_OVERLAP)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    vectordb = Chroma.from_documents(texts, embeddings, persist_directory=cfg.CHROMA_PATH)
+
+if __name__ == "__main__":
+    run_db_build()
